2190e942d47398a057d6cded884e2ef00e12ddcf
pppeedrito <pedro1996616@gmail.com>
2016-11-18 20:35:11 +0100

    bien

--- Convertidar_Monedas/Convertidar_Monedas/Form1.Designer.vb
+++ Convertidar_Monedas/Convertidar_Monedas/Form1.Designer.vb
@@ -24,22 +24,26 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.btnConv = New System.Windows.Forms.Button()
         Me.btn1 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
-        Me.Button6 = New System.Windows.Forms.Button()
-        Me.Button7 = New System.Windows.Forms.Button()
-        Me.Button8 = New System.Windows.Forms.Button()
-        Me.Button9 = New System.Windows.Forms.Button()
-        Me.Button10 = New System.Windows.Forms.Button()
-        Me.Button11 = New System.Windows.Forms.Button()
-        Me.Button12 = New System.Windows.Forms.Button()
-        Me.Button13 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnCambiar = New System.Windows.Forms.Button()
+        Me.btnComa = New System.Windows.Forms.Button()
+        Me.txtCalc = New System.Windows.Forms.TextBox()
+        Me.ComboObligatorio = New System.Windows.Forms.ComboBox()
+        Me.btnEli_Ulti = New System.Windows.Forms.Button()
+        Me.btnC = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnConv
         '
-        Me.btnConv.Location = New System.Drawing.Point(41, 26)
+        Me.btnConv.Location = New System.Drawing.Point(34, 12)
         Me.btnConv.Name = "btnConv"
         Me.btnConv.Size = New System.Drawing.Size(75, 23)
         Me.btnConv.TabIndex = 0
@@ -48,147 +52,190 @@ Partial Class Form1
         '
         'btn1
         '
-        Me.btn1.Location = New System.Drawing.Point(158, 37)
+        Me.btn1.Location = New System.Drawing.Point(34, 55)
         Me.btn1.Name = "btn1"
-        Me.btn1.Size = New System.Drawing.Size(79, 32)
+        Me.btn1.Size = New System.Drawing.Size(61, 23)
         Me.btn1.TabIndex = 1
-        Me.btn1.Text = "primero"
+        Me.btn1.Text = "1"
         Me.btn1.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btn3
         '
-        Me.Button3.Location = New System.Drawing.Point(255, 37)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btn3.Location = New System.Drawing.Point(165, 55)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(61, 24)
+        Me.btn3.TabIndex = 2
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
         '
-        'Button4
+        'btn2
         '
-        Me.Button4.Location = New System.Drawing.Point(199, 221)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(75, 23)
-        Me.Button4.TabIndex = 3
-        Me.Button4.Text = "Button4"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.btn2.Location = New System.Drawing.Point(101, 56)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(58, 23)
+        Me.btn2.TabIndex = 3
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(199, 186)
+        Me.Button5.Location = New System.Drawing.Point(34, 84)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.Size = New System.Drawing.Size(61, 24)
         Me.Button5.TabIndex = 4
-        Me.Button5.Text = "Button5"
+        Me.Button5.Text = "4"
         Me.Button5.UseVisualStyleBackColor = True
         '
-        'Button6
+        'btn6
         '
-        Me.Button6.Location = New System.Drawing.Point(207, 194)
-        Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(75, 23)
-        Me.Button6.TabIndex = 5
-        Me.Button6.Text = "Button6"
-        Me.Button6.UseVisualStyleBackColor = True
+        Me.btn6.Location = New System.Drawing.Point(165, 84)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(61, 24)
+        Me.btn6.TabIndex = 5
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
         '
-        'Button7
+        'btn5
         '
-        Me.Button7.Location = New System.Drawing.Point(215, 202)
-        Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(75, 23)
-        Me.Button7.TabIndex = 6
-        Me.Button7.Text = "Button7"
-        Me.Button7.UseVisualStyleBackColor = True
+        Me.btn5.Location = New System.Drawing.Point(101, 84)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(58, 23)
+        Me.btn5.TabIndex = 6
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
         '
-        'Button8
+        'btn7
         '
-        Me.Button8.Location = New System.Drawing.Point(223, 210)
-        Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(75, 23)
-        Me.Button8.TabIndex = 7
-        Me.Button8.Text = "Button8"
-        Me.Button8.UseVisualStyleBackColor = True
+        Me.btn7.Location = New System.Drawing.Point(34, 114)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(61, 23)
+        Me.btn7.TabIndex = 7
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
         '
-        'Button9
+        'btn8
         '
-        Me.Button9.Location = New System.Drawing.Point(231, 218)
-        Me.Button9.Name = "Button9"
-        Me.Button9.Size = New System.Drawing.Size(75, 23)
-        Me.Button9.TabIndex = 8
-        Me.Button9.Text = "Button9"
-        Me.Button9.UseVisualStyleBackColor = True
+        Me.btn8.Location = New System.Drawing.Point(101, 113)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(58, 23)
+        Me.btn8.TabIndex = 8
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
         '
-        'Button10
+        'btn0
         '
-        Me.Button10.Location = New System.Drawing.Point(239, 226)
-        Me.Button10.Name = "Button10"
-        Me.Button10.Size = New System.Drawing.Size(75, 23)
-        Me.Button10.TabIndex = 9
-        Me.Button10.Text = "Button10"
-        Me.Button10.UseVisualStyleBackColor = True
+        Me.btn0.Location = New System.Drawing.Point(34, 143)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(61, 23)
+        Me.btn0.TabIndex = 9
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
         '
-        'Button11
+        'btn9
         '
-        Me.Button11.Location = New System.Drawing.Point(247, 234)
-        Me.Button11.Name = "Button11"
-        Me.Button11.Size = New System.Drawing.Size(75, 23)
-        Me.Button11.TabIndex = 10
-        Me.Button11.Text = "Button11"
-        Me.Button11.UseVisualStyleBackColor = True
+        Me.btn9.Location = New System.Drawing.Point(165, 113)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(61, 23)
+        Me.btn9.TabIndex = 10
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
         '
-        'Button12
+        'btnCambiar
         '
-        Me.Button12.Location = New System.Drawing.Point(255, 242)
-        Me.Button12.Name = "Button12"
-        Me.Button12.Size = New System.Drawing.Size(75, 23)
-        Me.Button12.TabIndex = 11
-        Me.Button12.Text = "Button12"
-        Me.Button12.UseVisualStyleBackColor = True
+        Me.btnCambiar.Location = New System.Drawing.Point(255, 55)
+        Me.btnCambiar.Name = "btnCambiar"
+        Me.btnCambiar.Size = New System.Drawing.Size(121, 37)
+        Me.btnCambiar.TabIndex = 11
+        Me.btnCambiar.Text = "Cambiar"
+        Me.btnCambiar.UseVisualStyleBackColor = True
         '
-        'Button13
+        'btnComa
         '
-        Me.Button13.Location = New System.Drawing.Point(263, 250)
-        Me.Button13.Name = "Button13"
-        Me.Button13.Size = New System.Drawing.Size(75, 23)
-        Me.Button13.TabIndex = 12
-        Me.Button13.Text = "Button13"
-        Me.Button13.UseVisualStyleBackColor = True
+        Me.btnComa.Location = New System.Drawing.Point(319, 12)
+        Me.btnComa.Name = "btnComa"
+        Me.btnComa.Size = New System.Drawing.Size(61, 23)
+        Me.btnComa.TabIndex = 12
+        Me.btnComa.Text = "Coma"
+        Me.btnComa.UseVisualStyleBackColor = True
+        '
+        'txtCalc
+        '
+        Me.txtCalc.Location = New System.Drawing.Point(125, 12)
+        Me.txtCalc.Name = "txtCalc"
+        Me.txtCalc.Size = New System.Drawing.Size(188, 22)
+        Me.txtCalc.TabIndex = 13
+        '
+        'ComboObligatorio
+        '
+        Me.ComboObligatorio.FormattingEnabled = True
+        Me.ComboObligatorio.Items.AddRange(New Object() {"Dolares", "Yenes", "Dragmas", "Libras"})
+        Me.ComboObligatorio.Location = New System.Drawing.Point(255, 98)
+        Me.ComboObligatorio.Name = "ComboObligatorio"
+        Me.ComboObligatorio.Size = New System.Drawing.Size(121, 24)
+        Me.ComboObligatorio.TabIndex = 14
+        '
+        'btnEli_Ulti
+        '
+        Me.btnEli_Ulti.Location = New System.Drawing.Point(102, 143)
+        Me.btnEli_Ulti.Name = "btnEli_Ulti"
+        Me.btnEli_Ulti.Size = New System.Drawing.Size(57, 23)
+        Me.btnEli_Ulti.TabIndex = 15
+        Me.btnEli_Ulti.Text = "ELI_ULT"
+        Me.btnEli_Ulti.UseVisualStyleBackColor = True
+        '
+        'btnC
+        '
+        Me.btnC.Location = New System.Drawing.Point(165, 142)
+        Me.btnC.Name = "btnC"
+        Me.btnC.Size = New System.Drawing.Size(61, 24)
+        Me.btnC.TabIndex = 16
+        Me.btnC.Text = "C"
+        Me.btnC.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(424, 346)
-        Me.Controls.Add(Me.Button13)
-        Me.Controls.Add(Me.Button12)
-        Me.Controls.Add(Me.Button11)
-        Me.Controls.Add(Me.Button10)
-        Me.Controls.Add(Me.Button9)
-        Me.Controls.Add(Me.Button8)
-        Me.Controls.Add(Me.Button7)
-        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.btnC)
+        Me.Controls.Add(Me.btnEli_Ulti)
+        Me.Controls.Add(Me.ComboObligatorio)
+        Me.Controls.Add(Me.txtCalc)
+        Me.Controls.Add(Me.btnComa)
+        Me.Controls.Add(Me.btnCambiar)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn6)
         Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn3)
         Me.Controls.Add(Me.btn1)
         Me.Controls.Add(Me.btnConv)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents btnConv As Button
     Friend WithEvents btn1 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn2 As Button
     Friend WithEvents Button5 As Button
-    Friend WithEvents Button6 As Button
-    Friend WithEvents Button7 As Button
-    Friend WithEvents Button8 As Button
-    Friend WithEvents Button9 As Button
-    Friend WithEvents Button10 As Button
-    Friend WithEvents Button11 As Button
-    Friend WithEvents Button12 As Button
-    Friend WithEvents Button13 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnCambiar As Button
+    Friend WithEvents btnComa As Button
+    Friend WithEvents txtCalc As TextBox
+    Friend WithEvents ComboObligatorio As ComboBox
+    Friend WithEvents btnEli_Ulti As Button
+    Friend WithEvents btnC As Button
 End Class
--- Convertidar_Monedas/Convertidar_Monedas/Form1.vb
+++ Convertidar_Monedas/Convertidar_Monedas/Form1.vb
@@ -1,13 +1,75 @@
 ﻿Public Class Form1
-    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+    Dim euros As Double
+    Dim yens As Double
+    Dim dollars As Double
+    Dim dragmes As Double
+    Dim librasEst As Double
+    Dim operacion As Double
+    Dim primeraYens As Boolean
+    Dim primeraDoll As Boolean
+    Dim primeraDrag As Boolean
+    Dim primeraLibra As Boolean
+    Dim contDecimal As Integer = 0
+    Dim coma As Boolean = False
+    Dim i As Double
 
+
+    Private Sub btnConv_Click(sender As Object, e As EventArgs) Handles btnConv.Click
+        primeraDoll = False
+        primeraDrag = False
+        primeraLibra = False
+        primeraYens = False
+        i = ComboObligatorio.SelectedIndex
+        Select Case i
+            Case 0
+                If primeraDoll = False Then
+                    Try
+                        dollars = InputBox("¿Cuanto vale el dolar?")
+                        primeraDoll = CDbl(txtCalc.Text) * dollars
+                        txtCalc.Text = Format(operacion, "0.00")
+                    Catch
+                        MsgBox("Introduce un número correcto.")
+                    End Try
+                End If
+            Case 1
+                If primeraYens = False Then
+                    Try
+                        dollars = InputBox("¿Cuanto vale un yen?")
+                        primeraYens = CDbl(txtCalc.Text) * yens
+                        txtCalc.Text = Format(operacion, "0.00")
+                    Catch
+                        MsgBox("Introduce un número correcto.")
+                    End Try
+
+                End If
+            Case 2
+                If primeraDrag = False Then
+                    Try
+                        dragmes = InputBox("¿Cuanto vale el Dragmas?")
+                        primeraDoll = CDbl(txtCalc.Text) * dragmes
+                        txtCalc.Text = Format(operacion, "0.00")
+                    Catch
+                        MsgBox("Introduce un número correcto para la convercion a dragmas.")
+                    End Try
+
+                End If
+            Case 3
+                If primeraLibra = False Then
+                    Try
+                        librasEst = InputBox("¿Cuanto vale el Libras?")
+                        primeraDoll = CDbl(txtCalc.Text) * librasEst
+                        txtCalc.Text = Format(operacion, "0.00")
+                    Catch
+                        MsgBox("Introduce un número correcto.")
+                    End Try
+
+                End If
+
+        End Select
     End Sub
 
-    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
-
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btn1.Click
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboObligatorio.SelectedIndex = 0
 
     End Sub
 End Class
